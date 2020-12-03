239b559053703f889fb211f3741f509aa78af45e
DESKTOP-DRC0DO8\graction <dhk0561@naver.com>
2020-12-03 16:46:18 +0900

    [v1.6.4] Log with current time

--- src/main/java/org/nextwin/util/Logger.java
+++ src/main/java/org/nextwin/util/Logger.java
@@ -1,9 +1,16 @@
 package org.nextwin.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Logger {
+	
+	private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public static void log(String tag, String message) {
-		System.out.println("[" + tag + "]  " + message);
+		Date now = new Date();
+		String time = format.format(now);
+		System.out.println(time + " [" + tag + "]  " + message);
 	}
 	
 }
